cdc07f60f43a6516674f03f76c26ef2d44de8a59
jsmedina <jsmedina90@hotmail.com>
2013-05-31 09:08:24 -0500

    Modifique el title del menu

--- includes/menu.php
+++ includes/menu.php
@@ -1,9 +1,9 @@
 <div id="menu-wrap">
       <ul>
         <li><a href="index.php" title="Inicio">home</a></li>
-        <li><a href="measuring.php" title="Mediciones">about</a></li>
-        <li><a href="maps.php" title="Mapas">pricing</a></li>
-        <li><a href="graphs.php" title="Gráficos">clients</a></li>
+        <li><a href="measuring.php" title="about">about</a></li>
+        <li><a href="maps.php" title="pricing">pricing</a></li>
+        <li><a href="graphs.php" title="clients">clients</a></li>
         <div class="clear"></div>
       </ul>
     </div>
